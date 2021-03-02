21eb7a91f9447dd41b19e436fe43b3ab19f27bc2
Diego <consultoresartificialbrain@gmail.com>
2021-03-01 19:12:03 -0600

    agregando todo

--- src/controllers/pedido.controllers.js
+++ src/controllers/pedido.controllers.js
@@ -149,20 +149,20 @@ pedidoCtrl.generatePedidoPagado = async (req,res) => {
                     </tr>
                     ${pedidos}
                 </table>
-                <Button style=" 
-                    background-color: Blue;
-                    border: none;
-                    color: white;
-                    padding: 15px 32px;
-                    text-align: center;
-                    text-decoration: none;
-                    display: inline-block;
-                    font-size: 16px;
-                    margin: 4px 2px;
-                    cursor: pointer;
-                    text-decoration:none;
-                ">
-                        <a href="https://krispychicken.mx/admin/pedidos"> Ir a pedidos</a>
+                <Button>
+                        <a style=" 
+                        background-color: Blue;
+                        border: none;
+                        color: white;
+                        padding: 15px 32px;
+                        text-align: center;
+                        text-decoration: none;
+                        display: inline-block;
+                        font-size: 16px;
+                        margin: 4px 2px;
+                        cursor: pointer;
+                        text-decoration:none;
+                    " href="https://krispychicken.mx/admin/pedidos"> Ir a pedidos</a>
                 </Button>
             </div>
         </div>
